21decc3b22388af36f6618304823b73f5d25fbb1
LigarAlexandri <alexandriligar@gmail.com>
2024-03-29 21:14:47 +0700

    Tugas PWEB A PR CRUD_1001

---
+++ src/JS/Javascript.js
@@ -0,0 +1,9 @@
+// script.js WORK IN PROGRESS
+document.getElementById("loginButton").addEventListener("click", function() {
+    window.location.href = "sidebar.html";
+  });
+
+  document.getElementById("registerButton").addEventListener("click", function() {
+    window.location.href = "indexregis.html";
+  });
+  
